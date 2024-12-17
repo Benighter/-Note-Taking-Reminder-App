589b72117aa1ce67abea19d9fa00a49a739f516f
Benighter <111303968+Benighter@users.noreply.github.com.>
2024-12-17 08:02:28 +0200

    ...

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NoteListComponent } from './components/note-list/note-list.component';
+import { NotificationComponent } from './components/notification/notification.component';
 
 export const routes: Routes = [
   {path: 'navbar', component: NavbarComponent},
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  {path: 'login', component: LoginComponent },
+  {path: 'register', component: RegisterComponent },
+  {path: 'note-list', component: NoteListComponent},
+  {path: 'notification', component: NotificationComponent},
+
   // { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
