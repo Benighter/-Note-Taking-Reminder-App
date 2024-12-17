be7abb6ef994a9357e8cbf1ac9f9941891873730
Benighter <111303968+Benighter@users.noreply.github.com.>
2024-12-17 08:02:23 +0200

    ...

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  openNav() {
+    const sidenav = document.getElementById("mySidenav");
+    const main = document.getElementById("main");
+    if (sidenav) {
+      sidenav.style.width = "250px";
+    }
+    if (main) {
+      main.style.marginLeft = "250px";
+    }
+  }
 
+  closeNav() {
+    const sidenav = document.getElementById("mySidenav");
+    const main = document.getElementById("main");
+    if (sidenav) {
+      sidenav.style.width = "0";
+    }
+    if (main) {
+      main.style.marginLeft = "0";
+    }
+  }
 }
