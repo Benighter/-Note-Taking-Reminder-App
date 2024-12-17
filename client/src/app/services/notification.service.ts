275c793d83b76494ae5b29703251498c6150787c
Benighter <111303968+Benighter@users.noreply.github.com.>
2024-12-17 10:08:26 +0200

    fixed saving and deleting notes

--- client/src/app/services/notification.service.ts
+++ client/src/app/services/notification.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
+  private messageSource = new Subject<string>();
+  message$ = this.messageSource.asObservable();
 
   constructor() { }
 
   showSuccess(message: string) {
-    console.log(`Success: ${message}`);
+    this.messageSource.next(message);
   }
 }
