4169d345a43bf6e2931b5056eda2125be7417bfb
Benighter <111303968+Benighter@users.noreply.github.com.>
2024-12-17 08:28:09 +0200

    added card

--- client/src/app/components/note-list/note-list.component.ts
+++ client/src/app/components/note-list/note-list.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './note-list.component.css'
 })
 export class NoteListComponent {
-
+  openNote() {
+    // Add your logic here to open the note
+    console.log('Note clicked');
+  }
 }
